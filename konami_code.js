fb10903dfff0b883c1f12f55a169497a33d62564
Lisa Lee <snickers495@gmail.com>
2018-01-15 21:09:48 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,10 +2,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
-  // Write your JavaScript code inside the init() function
   let index = 0;
   const body = document.querySelector('body');
   body.addEventListener('keydown', function(e) {
@@ -21,3 +17,4 @@ function init() {
     }
   });
 }
+
